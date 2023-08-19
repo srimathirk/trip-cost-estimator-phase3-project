f619d3c7d0cc9246c13bed2eed2d411af7ee2116
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-08-19 12:23:04 -0400

    adding users table

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,9 +1,18 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Table, ForeignKey, MetaData
+from sqlalchemy import create_engine, MetaData, func, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Table, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.associationproxy import association_proxy
 
+#database connection
 engine = create_engine('sqlite:///trip_records.db')
-
 Base = declarative_base()
+#User class (table)
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer(), primary_key=True)
+    user_name = Column(String())
+    Email = Column(String())
+    Phone = Column(String())
+    created_at = Column(DateTime(),server_default=func.now())
+    
